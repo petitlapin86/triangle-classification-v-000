942db9c3f88a7466a25c4018be71db6124211aea
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 20:08:20 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,4 +5,19 @@ class Triangle
     @b = b
     @c = c
   end
+
+
+  def kind
+  validate_triangle #pulls in other instance method
+  if a == b && b == c #if all sides are equal, its equilateral
+    :equilateral
+  elsif a == b || b == c || a == c # if two sides are equal its isosceles
+    :isosceles
+  else
+    :scalene  #if no sides are the same its scalene
+  end
+end
+  end 
+
+  
 end
