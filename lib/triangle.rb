d24130f34d00a0d80d05016c6612c5deaa8fcfa6
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 20:11:53 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,17 +16,17 @@ class Triangle
   else
     :scalene  #if no sides are the same its scalene
   end #end if else
-  end #end kind 
+  end #end kind
 
   def validate_triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
     [a, b, c].each { |s| real_triangle << false if s <= 0 }
-    #kind error will raise a custom error if the triangle is invalid 
+    #kind error will raise a custom error if the triangle is invalid
     raise TriangleError if real_triangle.include?(false)
-end 
+end
 
 
 class TriangleError < StandardError #triangle error is inherited from StandardError
 end
 
-end # end class 
+end # end class
